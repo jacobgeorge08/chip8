dc9a5b15c515af66f0fe38f2aaa37b567b9c31d1
T460 <jacobgeorge08@gmail.com>
2026-08-08 14:51:34 +0530

    added basic fontset + emu reset fn

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -8,33 +8,64 @@ const NUM_KEYS: usize = 16;
 
 const START_ADDR: u16 = 0x200;
 
+const FONTSET_SIZE: usize = 80; // 16 hex chars * 5 bytes/char
+const FONTSET: [u8; FONTSET_SIZE] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
+
+// 0 sprite after hexadecimal is converted to binary. Looks like 0 if you squint :P
+// 11110000
+// 10010000
+// 10010000
+// 10010000
+// 11110000
+
 pub struct Emu {
     pc: u16,
     ram: [u8; RAM_SIZE],
     screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
     v_reg: [u8; NUM_REGS],
     i_reg: u16,
-    stack: [u16; STACK_SIZE],
     sp: u16,
+    stack: [u16; STACK_SIZE],
     keys: [bool; NUM_KEYS],
     dt: u8, // delay timer
     st: u8, // sound timer
 }
 
 impl Emu {
-    fn new() -> Self {
-        Self {
+    pub fn new() -> Self {
+        let mut new_emu = Self {
             pc: START_ADDR,
             ram: [0; RAM_SIZE],
             screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
             v_reg: [0; NUM_REGS],
             i_reg: 0,
-            stack: [0; STACK_SIZE],
             sp: 0,
+            stack: [0; STACK_SIZE],
             keys: [false; NUM_KEYS],
             dt: 0,
             st: 0,
-        }
+        };
+
+        new_emu.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+
+        new_emu
     }
 
     fn push(&mut self, val: u16) {
@@ -45,5 +76,19 @@ impl Emu {
     fn pop(&mut self) -> u16 {
         self.sp -= 1;
         self.stack[self.sp as usize]
+    }
+
+    pub fn reset(&mut self) {
+        self.pc = START_ADDR;
+        self.ram = [0; RAM_SIZE];
+        self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
+        self.v_reg = [0; NUM_REGS];
+        self.i_reg = 0;
+        self.sp = 0;
+        self.stack = [0; STACK_SIZE];
+        self.keys = [false; NUM_KEYS];
+        self.dt = 0;
+        self.st = 0;
+        self.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
     }
 }
