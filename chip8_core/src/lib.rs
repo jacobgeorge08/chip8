dac6f14d04f13ef0d585a8f96f73950cf5177e2b
T460 <jacobgeorge08@gmail.com>
2026-08-08 13:14:36 +0530

    emulator init

---
+++ chip8_core/src/lib.rs
@@ -0,0 +1,39 @@
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 32;
+
+const RAM_SIZE: usize = 4096;
+const NUM_REGS: usize = 16;
+const STACK_SIZE: usize = 16;
+const NUM_KEYS: usize = 16;
+
+const START_ADDR: u16 = 0x200;
+
+pub struct Emu {
+    pc: u16,
+    ram: [u8; RAM_SIZE],
+    screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
+    v_reg: [u8; NUM_REGS],
+    i_reg: u16,
+    stack: [u16; STACK_SIZE],
+    sp: u16,
+    keys: [bool; NUM_KEYS],
+    dt: u8, // delay timer
+    st: u8, // sound timer
+}
+
+impl Emu {
+    fn new() -> Self {
+        Self {
+            pc: START_ADDR,
+            ram: [0; RAM_SIZE],
+            screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
+            v_reg: [0; NUM_REGS],
+            i_reg: 0,
+            stack: [0; STACK_SIZE],
+            sp: 0,
+            keys: [false; NUM_KEYS],
+            dt: 0,
+            st: 0,
+        }
+    }
+}
