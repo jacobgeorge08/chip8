f5e613f4d245e43f6af8ba3931f2f86e51f4c062
T460 <jacobgeorge08@gmail.com>
2026-08-13 03:15:03 +0530

    formatting fix

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -133,7 +133,7 @@ impl Emu {
         let digit1 = (op & 0xF000) >> 12;
         let digit2 = (op & 0x0F00) >> 8;
         let digit3 = (op & 0x00F0) >> 4;
-        let digit4 = (op & 0x000F);
+        let digit4 = op & 0x000F;
 
         match (digit1, digit2, digit3, digit4) {
             // No Operation
